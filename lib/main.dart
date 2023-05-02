c372342dbe19fdd66219fe2933c1f3b16620d093
Q-T5 <bikathimartin@outlook.com>
2023-05-02 16:07:12 +0300

    Complete calculator logic

--- lib/main.dart
+++ lib/main.dart
@@ -15,52 +15,163 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int num1 = 0;
-  int num2 = 0;
-  int results = 0;
+  String num1 = "";
+  String num2 = "";
+  String result = "0";
   String operation = "+";
+
+  final List<String> operatorList = ["+", "-", "/", "*"];
+
+  // text editting controllers
+  late final TextEditingController _num1Controller;
+  late final TextEditingController _num2Controller;
+
+  @override
+  void initState() {
+    _num1Controller = TextEditingController();
+    _num2Controller = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _num1Controller.dispose();
+    _num2Controller.dispose();
+    super.dispose();
+  }
 
   void resetNumbers() {
     setState(() {
-      num1 = 0;
-      num2 = 0;
+      _num1Controller.text = "";
+      _num2Controller.text = "";
+      result = "0";
     });
   }
 
-  void calculate(String operator) {
+  void calculate() {
     switch (operation) {
       case "+":
         setState(() {
-          results = num1 + num2;
+          result = (int.parse(_num1Controller.text) +
+                  int.parse(_num2Controller.text))
+              .toString();
         });
         break;
       case "-":
         setState(() {
-          results = num1 - num2;
+          result = (int.parse(_num1Controller.text) -
+                  int.parse(_num2Controller.text))
+              .toString();
         });
         break;
       case "/":
         setState(() {
-          results = (num1 / num2) as int;
+          result = (int.parse(_num1Controller.text) /
+                  int.parse(_num2Controller.text))
+              .toString();
         });
         break;
       case "*":
         setState(() {
-          results = num1 * num2;
+          result = (int.parse(_num1Controller.text) *
+                  int.parse(_num2Controller.text))
+              .toString();
         });
     }
+  }
+
+  void setOperator(String operator) {
+    setState(() {
+      operation = operator;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
         title: const Text("Simple Flutter Calculator"),
         centerTitle: true,
         backgroundColor: Colors.redAccent,
       ),
-      body: const Center(
-        child: Text("my data"),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                controller: _num1Controller,
+                decoration: const InputDecoration(
+                    hintText: "e.g 10",
+                    labelText: "Enter First Number",
+                    labelStyle: TextStyle(color: Colors.redAccent)),
+                enableSuggestions: false,
+                keyboardType: TextInputType.number,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Text(
+                  "Operator: $operation",
+                  style: const TextStyle(
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: operatorList.map((operator) {
+                  return OutlinedButton(
+                    onPressed: () {
+                      setOperator(operator);
+                    },
+                    style: const ButtonStyle(
+                      foregroundColor: MaterialStatePropertyAll(Colors.red),
+                    ),
+                    child: Text(operator),
+                  );
+                }).toList(),
+              ),
+              TextField(
+                controller: _num2Controller,
+                decoration: const InputDecoration(
+                    hintText: "e.g 10",
+                    labelText: "Enter Second Number",
+                    labelStyle: TextStyle(color: Colors.redAccent)),
+                enableSuggestions: false,
+                keyboardType: TextInputType.number,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: OutlinedButton(
+                      onPressed: () {
+                        calculate();
+                      },
+                      style: const ButtonStyle(
+                        foregroundColor: MaterialStatePropertyAll(Colors.red),
+                      ),
+                      child: const Text(
+                        "=",
+                        style: TextStyle(fontSize: 25),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "Results: $result",
+                  style: const TextStyle(fontSize: 20.0),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
